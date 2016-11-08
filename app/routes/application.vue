b5bf1c22f90255a3c2b4912e0b9b561d5e3235b0
Dustin Walker <dwtimp@gmail.com>
2016-11-08 14:52:35 -0600

    base html

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -8,24 +8,28 @@
           <p class="panel-heading title">
             Sign Up For My Web App
           </p>
-          <p v-for"item in formInputs" v-if="item.type !== 'select' && item.type !== 'textarea' " class="control has-icon has-icon-left">
-            <i class="fa fa-user-o" aria-hidden="true"></i>
-            <input v-model="formValues[formInputs.id]" class="input" type="text" :placeholder="item.label">
-          </p>
-          <p v-for"item in formInputs" v-if="item.type === 'select'" class="control has-icon has-icon-left">
-            <span class="select is-fullwidth">
-              <select v-model="formValues[formInputs.id]" :placeholder="item.label">
-                <option v-for="option in item.options" value="">{{ option.label }}</option>
-              </select>
-            </span>
-          </p>
 
-          <p v-for="item in formInputs" v-if="item.type === 'textarea' " class="control has-icon has-icon-left">
-            <i class="fa fa-comments-o" aria-hidden="true"></i>
-            <textarea class="textarea input" :placeholder="item.label" v-model="formValues[formInputs.id]"></textarea>
-          </p>
+          <div class="panel-block">
+            <p class="control has-icon has-icon-left">
+              <i class="fa fa-user-o" aria-hidden="true"></i>
+              <input class="input" type="text" placeholder="item.label">
+            </p>
+            <p class="control has-icon has-icon-left">
+              <span class="select is-fullwidth">
+                <select placeholder="item.label">
+                  <option value="">{O label}</option>
+                  <option value="">{O label}</option>
+                </select>
+              </span>
+            </p>
 
-          <p class="control is-fullwidth">
+            <p class="control has-icon has-icon-left">
+              <i class="fa fa-comments-o" aria-hidden="true"></i>
+              <textarea class="textarea input"></textarea>
+            </p>
+          </div>
+
+          <p class="panel-block">
             <button @click="submitForm(formValues)" class="button is-fullwidth is primary">Submit</button>
           </p>
 
@@ -37,19 +41,14 @@
   </div>
 </template>
 
-
-
-
-
-
 <script>
 const apiUrl = 'http://json-data.herokuapp.com/forms'
 export default {
   data() {
     return {
       apiUrl,
-      formImputs: [],
-      formValues: [],
+      formInputs: [],
+      formValues: {},
     };
   },
 
@@ -58,7 +57,7 @@ export default {
   },
 
   methods: {
-    getdata() {
+    getData() {
       fetch(apiUrl)
         .then((r) => r.json())
         .then((formInputs) => {
